2f995ee4efd121960cde491b7b2a6ec5090ec535
John Ciccone <cicconej93@gmail.com>
2018-12-15 21:10:34 -0500

    add test for feed name length and definition

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,14 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have a non-empty name', function(){
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            })
+        })
+
+
     });
 
 
