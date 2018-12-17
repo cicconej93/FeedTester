05ebbf6c51381ed7a50dff823550238d96bd2314
John Ciccone <cicconej93@gmail.com>
2018-12-16 23:21:36 -0500

    refactor function syntax to ES6

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('have a non-empty URL', function(){
+        it('have a non-empty URL', () => {
             allFeeds.forEach ( feed => { 
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
@@ -42,28 +42,28 @@ $(function() {
         * This test loops through each feed in allFeeds to ensure that
         * a name is defined, and is not empty
         */
-        it('have a non-empty name', function(){
+        it('have a non-empty name', () => {
             allFeeds.forEach(feed => {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
-            })
-        })
+            });
+        });
 
 
     });
 
 
     //Test suite for the menu
-    describe('The menu', function(){
+    describe('The menu', () => {
         let menu;
 
-        beforeEach(function(){
+        beforeEach(() => {
             menu = document.querySelector("body");
         });
          /**
           *  This test ensures that the menu is hidden by default
           */
-         it('is hidden by default', function(){
+         it('is hidden by default', () => {
              expect(menu.classList.contains("menu-hidden")).toBe(true);
          });
 
@@ -71,7 +71,7 @@ $(function() {
           * Test to ensure menu visibility changes when icon is clicked.
           */
 
-         it('is visible when icon clicked, if previously not visible', function(){
+         it('is visible when icon clicked, if previously not visible', () => {
             //select our menuIcon
             let menuIcon = document.querySelector(".menu-icon-link");
 
@@ -85,7 +85,7 @@ $(function() {
     });
 
     /* Test suite for the Initial Entries */
-    describe('Initial Entries', function(){
+    describe('Initial Entries', () => {
 
         /**
         * Tests to ensure there is at least a single entry
@@ -94,12 +94,12 @@ $(function() {
 
         //call the loadFeed function and let it complete before running test
         //done is passed as a callback function here
-        beforeEach(function(done){
+        beforeEach((done) => {
             loadFeed(0, done);
            
         });
 
-        it('should exist and contain at least 1 entry', function(){
+        it('should exist and contain at least 1 entry', () => {
             let entryLink = document.querySelector('.entry-link');
             
             expect(entryLink).toBeDefined();
@@ -110,7 +110,7 @@ $(function() {
     });
 
     /* Test for the selecting a new feed*/
-    describe('New Feed Selection', function(){
+    describe('New Feed Selection', () => {
         const feed = document.querySelector('.feed');
         let firstFeed, secondFeed;
 
@@ -118,12 +118,11 @@ $(function() {
          * Tests that ensures a new feed is loaded, i.e. the content
          * actually changes once loadFeed function is called.
          */
-        beforeEach(done => {
+        beforeEach( (done) => {
             //get first feed loaded, store URL feed is pulling from
             //tried to incorporate arrow functions here as practice.
             loadFeed(0, () => {
                 firstFeed = feed.innerText;
-                console.log(firstFeed);
                 
                 //get second feed loaded, store URL feed is pulling from
                 //each arrow function is a callback function in the loadFeed function
@@ -136,7 +135,7 @@ $(function() {
         });
 
         //test to check that feeds are changing once selected
-        it('loads a new feed and pulls from different feed URL', function() {
+        it('loads a new feed and pulls from different feed URL', () => {
             expect(firstFeed).not.toEqual(secondFeed);
         });
     
